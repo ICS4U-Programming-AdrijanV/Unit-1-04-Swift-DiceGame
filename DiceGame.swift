6fd089d2cfa298a0ed0914030e4551e7ed850c2d
Adrijan Vranjkovic <adrijan.vranjkovic@stu.ocsb.ca>
2023-02-27 19:12:40 +0000

    Fixed Error

--- DiceGame.swift
+++ DiceGame.swift
@@ -4,26 +4,43 @@ import Glibc
 
 // Get user input and set constants.
 print("Guess a number from 1-6?")
-var counter = 0
+var counter = 1
+
+// generate random number between 1 and 6.
 let randomInt = Int.random(in: 1..<7)
-if let input = Int(readLine()!) {
+
+// Try get integer from the user.
+if var input = Int(readLine()!) {
+
+    // While loop.
     while (input != randomInt) {
+
         counter = counter + 1
         // Check to see if the input was valid.
         if (input > 6 || input < 1) {
-
+            // Print if they enter a number out of range.
             print("That is not a valid guess.")
+            // get user input again.
+            input = Int(readLine()!)!
         } else if (input > randomInt) {
+            // Print too high.
             print("Too high.")
+            input = Int(readLine()!)!
         } else if (input < randomInt) {
+            // Print too low.
             print("Too low.")
+            input = Int(readLine()!)!
         } else {
+            // Print correct.
             print("Correct!")
         }
     }
+    // Print when while loop ends.
+    print("You took \(counter) guesses")
 } else {
+    // Print when user enters string.
     print("This is not a valid input!")
 }
-print("You took /(counter) guesses")
+
 
 
